ad198d249e1c5aca6891241d8097786179de1f2f
Vitaly Reutsky <vreutsky77@mail.ru>
2022-10-13 13:32:37 +0300

    Добавил страницу отдельного продукта

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,5 +1,6 @@
 // Реализация табов
 const sectionProducts = document.querySelector(".products");
+const sectionProduct = document.querySelector(".single-product");
 let tabs = document.querySelectorAll(".product__item-title"),
   tabsContent = document.querySelectorAll(".tab-content"),
   tabsParent = document.querySelector(".tab-header__items"),
@@ -25,6 +26,41 @@ if (sectionProducts) {
   function showTabContent(i = 0) {
     tabsContent[i].classList.add("show-tabs-content");
     tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tab-header__item--active");
+
+    tabsMenu[i].classList.add("show-tabs-menu");
+    tabsMenu[i].classList.remove("hide");
+  }
+
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", function (event) {
+    const target = event.target;
+    if (target.classList.contains("product__item-title")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+}
+
+if (sectionProduct) {
+  function hideTabContent() {
+    tabsMenu.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show-tabs-menu");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tab-header__item--active");
+    });
+  }
+
+  function showTabContent(i = 0) {
     tabs[i].classList.add("tab-header__item--active");
 
     tabsMenu[i].classList.add("show-tabs-menu");
