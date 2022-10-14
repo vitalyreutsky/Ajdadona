ffb12fe754cd280e53bb379316df6961c67190bb
Vitaly Reutsky <vreutsky77@mail.ru>
2022-10-14 17:54:58 +0300

    Изменил lang

---
+++ src/js/components/count.js
@@ -0,0 +1,38 @@
+//const countItemOne = document.querySelector(".count-results--one");
+//const countItemSecond = document.querySelector(".count-results--second");
+//const countThird = document.querySelector(".count-results--third");
+
+//const countValueOne = document.querySelector("[data-count-one]");
+//const countValueSecond = document.querySelector("[data-count-second]");
+//const countValueThird = document.querySelector("[data-count-third]");
+
+//const arrCountOne = [234, 360, 379, 460, 578, 978];
+//const arrCountTwo = [1234, 3890, 5360, 6825, 7379, 7460, 7578, 8560, 9978];
+//const arrCountThird = [234, 890, 360, 825, 379, 460, 578, 560, 978];
+
+//function printNumbers(arr, countItem, endNum) {
+//  i = 0;
+//  setTimeout(function run() {
+//    if (i < arr.length) {
+//      go(i, arr, countItem, endNum);
+//      i++;
+//      setTimeout(run, 500);
+//    }
+//  }, 500);
+//}
+
+//function go(i, arr, countItem, endNum) {
+//  if (arr[i] <= endNum) {
+//    countItem.innerHTML = arr[i];
+//  }
+//}
+
+//setTimeout(() => {
+//  printNumbers(arrCountOne, countItemOne, countValueOne.dataset.countOne);
+//}, 100);
+
+//printNumbers(
+//  arrCountTwo,
+//  countItemSecond,
+//  countValueSecond.dataset.countSecond
+//);
