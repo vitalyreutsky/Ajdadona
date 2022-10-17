5b8828aef74231ca36991c3456fc314d748fdfe8
Vitaly Reutsky <vreutsky77@mail.ru>
2022-10-17 13:08:02 +0300

    Закончил адаптив на 768px на странице about us

--- src/js/components/custom-scroll.js
+++ src/js/components/custom-scroll.js
@@ -1,4 +1,17 @@
 // Подключение плагина кастом-скролла
 import SimpleBar from "simplebar";
 
-new SimpleBar(document.querySelector(".products__header"), { autoHide: false });
+const sectionProductsScroll = document.querySelector(".products");
+const sectionProductScroll = document.querySelector(".single-product");
+
+if (sectionProductScroll) {
+  new SimpleBar(document.querySelector(".products__header"), {
+    autoHide: false,
+  });
+}
+
+if (sectionProductsScroll) {
+  new SimpleBar(document.querySelector(".products__header"), {
+    autoHide: false,
+  });
+}
