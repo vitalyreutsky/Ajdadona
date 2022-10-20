0e52161afb474ead3305f4ec9bcad1f485afab3d
Vitaly Reutsky <vreutsky77@mail.ru>
2022-10-20 12:15:57 +0300

    Добавил изменения в скрипт табов и добавил мелкие изменения по некоторым страницам

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,6 +1,5 @@
 // Реализация табов
 const sectionProducts = document.querySelector(".products");
-const sectionProduct = document.querySelector(".single-product");
 
 let tabs = document.querySelectorAll(".product__item-title"),
   tabsContent = document.querySelectorAll(".tab-content"),
@@ -47,12 +46,15 @@ let itemMenuOpen = document.querySelectorAll(".product__item-title");
 for (i = 0; i < itemMenuOpen.length; i++) {
   let subMenu = itemMenuOpen[i].nextElementSibling;
   let thisitemMenuOpen = itemMenuOpen[i];
+  let thisItemSvg = itemMenuOpen[i].querySelector("svg");
 
   itemMenuOpen[i].addEventListener("click", function () {
     subMenu.classList.toggle("show-tabs-menu");
     thisitemMenuOpen.classList.toggle("tab-header__item--active");
 
     subMenu.classList.toggle("fade");
+
+    thisItemSvg.classList.toggle("accordion__header-icon--active");
   });
 }
 
@@ -63,6 +65,9 @@ for (i = 0; i < itemSubMenuOpen.length; i++) {
       e.target.classList.toggle("tab-header__item--active");
       e.target.nextElementSibling.classList.toggle("show-tabs-menu");
       e.target.nextElementSibling.classList.toggle("fade");
+      e.target
+        .querySelector("svg")
+        .classList.toggle("accordion__header-icon--active");
     }
   });
 }
